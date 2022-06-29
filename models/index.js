6ecff929354908d28c6a014bf9c82948fc578bed
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-29 17:00:21 -0400

    switched aliases in index.js; added product associations in tag-routes.js GET methods

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'tagged_products'
+  as: 'product_tags'
 })
 
 // Tags belongToMany Products (through ProductTag)
@@ -30,7 +30,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tags'
+  as: 'tagged_products'
 })
 
 module.exports = {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,9 +5,10 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // TODO: be sure to include its associated Product data
   try{ 
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products'}]
+    });
     return res.status(200).json(tagData);
   } catch(err) {
     return res.status(500).json(err);
@@ -16,9 +17,10 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // TODO: be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(req.params.id);
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products'}]
+    });
     if (!tagData){
       return res.status(404).json({ message: 'No tag with this id.' });
     }
