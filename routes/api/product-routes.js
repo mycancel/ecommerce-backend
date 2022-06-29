3f435f389e331e5e55e3f13a560cf8bdadb8d6b3
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-28 22:43:22 -0400

    in product_routes.js, found all products in get method; selected product by id in get method; used destroy method in delete HTTP method

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  // TODO: be sure to include its associated Category and Tag data
+  try{ 
+    const productData = await Product.findAll();
+    return res.status(200).json(productData);
+  } catch(err) {
+    return res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id);
+    if (!productData){
+      return res.status(404).json({ message: 'No product with this id.' });
+    }
+    return res.status(200).json(productData);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +106,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.status(200).json(productData);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
