e3adf325f91654e3eccad6a5ff52d1f98e17d96d
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-29 17:05:01 -0400

    added product associations in product-routes.js GET methods

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,9 +6,13 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
-  // TODO: be sure to include its associated Category and Tag data
   try{ 
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [
+        { model: Tag, through: ProductTag, as: 'product_tags'},
+        { model: Category }
+      ]
+    });
     return res.status(200).json(productData);
   } catch(err) {
     return res.status(500).json(err);
@@ -18,9 +22,13 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.params.id);
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Tag, through: ProductTag, as: 'product_tags'},
+        { model: Category }
+      ]
+    });
     if (!productData){
       return res.status(404).json({ message: 'No product with this id.' });
     }
